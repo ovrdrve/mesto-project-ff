d332f1351e5ab4062d932f21da5f110fe4cc3d2d
Daniil Cheremisin <daniel.sugar@yandex.ru>
2024-06-26 00:10:21 +0300

    Created add & remove funcs

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
-// @todo: Функция создания карточки
+const addCard = (cardElement, callback) => {
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
 
-// @todo: Функция удаления карточки
+  card.querySelector(".card__image").src = cardElement.link;
+  card.querySelector(".card__title").textContent = cardElement.name;
+  card.querySelector(".card__delete-button").addEventListener("click", (e) => {
+    removeCard(e);
+  });
 
-// @todo: Вывести карточки на страницу
+  placesList.append(card);
+};
+
+const removeCard = (e) => {
+  e.target.closest(".card").remove();
+};
+
+for (let i = 0; i < initialCards.length; i++) {
+  addCard(initialCards[i], removeCard);
+}
